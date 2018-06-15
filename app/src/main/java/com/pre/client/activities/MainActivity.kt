a0559ab60034a1a375660503eaf5f14dbbbe48c4
BlueTeleco <lucas.lucario@gmail.com>
2018-06-15 12:38:57 +0200

    Fixed key encoding, changed to Base64

--- app/src/main/java/com/pre/client/activities/MainActivity.kt
+++ app/src/main/java/com/pre/client/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pre.client.activities
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.util.Log
 import android.view.View
 import com.pre.client.R
@@ -12,11 +13,13 @@ import kotlinx.coroutines.experimental.async
 
 import nics.crypto.proxy.afgh.AFGHGlobalParameters
 import nics.crypto.proxy.afgh.AFGHProxyReEncryption
+import org.apache.commons.codec.binary.Hex
 import org.jetbrains.anko.coroutines.experimental.bg
 import org.jetbrains.anko.startActivity
 import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.URLEncoder
 
 const val host = "10.0.2.2"
 var global: AFGHGlobalParameters? = null
@@ -50,18 +53,16 @@ class MainActivity : AppCompatActivity() {
         global = AFGHGlobalParameters(globalString)
         async(UI) {
             val sk = AFGHProxyReEncryption.generateSecretKey(global)
-
-            val uname = name_input.text
-            val phone = telefono_input.text
+            val sk64 = Base64.encodeToString(sk.toBytes(), Base64.DEFAULT)
             val pk = AFGHProxyReEncryption.generatePublicKey(sk, global)
+            val pk64 = Base64.encodeToString(pk.toBytes(), Base64.DEFAULT)
 
-            val pks = String(pk.toBytes())
+            val charset = "UTF-8"
+            val uname = URLEncoder.encode(name_input.text.toString(), charset)
+            val phone = URLEncoder.encode(telefono_input.text.toString(), charset)
+            val pks   = URLEncoder.encode(pk64, charset)
 
-            Log.e("pks ---> ", pks)
-            Log.e("Converted ---> ", pks)
-
-            save(uname.toString(), phone.toString())
-
+            save(uname.toString(), phone.toString(), sk64)
             bg {
                 val params = "uname=$uname&phone=$phone&pk=$pks"
                 val url = URL("http://$host:8080/new-user")
@@ -93,11 +94,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun save(user: String, phone: String) {
+    fun save(user: String, phone: String, sks: String) {
         val preferences = getSharedPreferences(getString(R.string.parameters_file), Context.MODE_PRIVATE)
         with(preferences.edit()) {
             putString(getString(R.string.user_phone), phone)
             putString(getString(R.string.user_name), user)
+            putString(getString(R.string.user_secret), sks)
             commit()
         }
     }
--- app/src/main/java/com/pre/client/activities/NewMemberActivity.kt
+++ app/src/main/java/com/pre/client/activities/NewMemberActivity.kt
@@ -1,18 +1,23 @@
 package com.pre.client.activities
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import android.view.View
 import com.pre.client.R
 import kotlinx.android.synthetic.main.activity_new_member.*
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.async
+import nics.crypto.proxy.afgh.AFGHProxyReEncryption
 import org.jetbrains.anko.clearTop
 import org.jetbrains.anko.coroutines.experimental.bg
 import org.jetbrains.anko.intentFor
 import java.io.OutputStreamWriter
 import java.net.HttpURLConnection
 import java.net.URL
+import java.net.URLEncoder
 
 class NewMemberActivity : AppCompatActivity() {
 
@@ -26,10 +31,19 @@ class NewMemberActivity : AppCompatActivity() {
     }
 
     fun addMember(view: View) {
+        val uphone = new_member.text.toString()
         async(UI) {
-            val uphone = new_member.text
+            val pubKeyStr = bg {
+                URL("http://$host:8080/public-key?phone=$uphone").readText()
+            }
+            val pk = Base64.decode(pubKeyStr.await(), Base64.DEFAULT)
+            val sk = getSecretKey()
+            val rk = AFGHProxyReEncryption.generateReEncryptionKey(pk, sk, global)
+            val rk64 = Base64.encodeToString(rk, Base64.DEFAULT)
+
             bg {
-                val params = "phone=$uphone"
+                val charset = "UTF-8"
+                val params = "phone=${URLEncoder.encode(uphone, charset)}&rk=${URLEncoder.encode(rk64, charset)}"
                 val url = URL("http://$host:8080/add-user/$idChat")
                 with(url.openConnection() as HttpURLConnection) {
                     requestMethod = "POST"
@@ -43,5 +57,12 @@ class NewMemberActivity : AppCompatActivity() {
             }
         }
         startActivity(intentFor<ConversationActivity>().clearTop())
+    }
+
+
+    fun getSecretKey(): ByteArray {
+        val sk64 = getSharedPreferences(getString(R.string.parameters_file), Context.MODE_PRIVATE)
+                 .getString(getString(R.string.user_secret), "")
+        return Base64.decode(sk64, Base64.DEFAULT)
     }
 }
